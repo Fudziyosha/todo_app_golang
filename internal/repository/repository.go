6973b15445169f55f88064f0a6fb86cb3087156e
Fudziyosha <maxleokryyt@gmail.com>
2026-03-29 03:23:09 +0300

    changed a long line of code

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -27,9 +27,15 @@ func NewRepository(postgres *PostgresConfig) *Repository {
 	return repository
 }
 func (r *Repository) Connect(ctx context.Context) error {
-	connConfig, err := pgx.ParseConfig(fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable", r.postgres.User, r.postgres.Password, r.postgres.Host, r.postgres.Port, r.postgres.DatabaseName))
+	connConfig, err := pgx.ParseConfig(fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable",
+		r.postgres.User,
+		r.postgres.Password,
+		r.postgres.Host,
+		r.postgres.Port,
+		r.postgres.DatabaseName))
 	if err != nil {
-		logrus.Error("Unable to connect to database ", err)
+		logrus.Error("database: failed parse config database ", err)
+		return err
 	}
 
 	connConfig.Tracer = &tracelog.TraceLog{
@@ -41,6 +47,7 @@ func (r *Repository) Connect(ctx context.Context) error {
 	connStr, err := pgx.ConnectConfig(ctx, connConfig)
 	if err != nil {
 		logrus.Error("database: failed connect to database ", err)
+		return err
 	}
 
 	r.database = connStr
